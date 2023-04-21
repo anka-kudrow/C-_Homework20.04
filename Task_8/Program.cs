cbceb0f14340c7513175cd73a9f24895486d8728
Anna <anka-kudrow@mail.ru>
2023-04-21 09:17:11 +0300

    Added Task8

---
+++ Task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Write number");
+int num = Convert.ToInt32(Console.ReadLine());
+count = 1;
+while (count <= num) 
+{
+    if(num % 2 == 0)
+    {
+        // КУДА КЛАСТЬ ЧЁТНЫЕ ЧИСЛА???
+    }
+    count += 1
+}
+Console.WriteLine();
+ 
+
+
+    
